cbf24382af9d0c8990c7e525e13dd831e01e21a6
Juan Carlos Alfaro <juan.alfaro.fer@gmail.com>
2026-06-01 23:14:20 -0500

    chore: setup project package structure

--- app/src/main/java/com/jotasedev/finwallet/MainActivity.kt
+++ app/src/main/java/com/jotasedev/finwallet/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.jotasedev.finwallet.ui.theme.FinWalletTheme
+import com.jotasedev.finwallet.core.design.theme.FinWalletTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/jotasedev/finwallet/core/design/theme/Theme.kt
+++ app/src/main/java/com/jotasedev/finwallet/core/design/theme/Theme.kt
@@ -1,6 +1,5 @@
-package com.jotasedev.finwallet.ui.theme
+package com.jotasedev.finwallet.core.design.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
